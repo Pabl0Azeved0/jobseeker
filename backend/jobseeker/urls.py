22f2ae53ce99b018f2ec4eb562fdb3e06a02b6e2
Pablo Azevedo <pabloslayer@gmail.com>
2025-03-19 15:58:57 -0300

    feat(jobseeker): Update settings for customUser, add beforehand the routes of other packages

--- backend/jobseeker/urls.py
+++ backend/jobseeker/urls.py
@@ -7,7 +7,10 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('accounts.urls')),
+    path('api/', include('applications.urls')),
     path('api/', include('jobs.urls')),
+    path('api/', include('profiles.urls')),
 
     # JWT auth endpoints
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
