c50b70acbc10b257448e2c39ec21055348f71c6b
Pablo Azevedo <pabloslayer@gmail.com>
2025-03-25 12:02:54 -0300

    hotfix(BE:applications): Refactor applications serializers to return more data

--- backend/applications/serializers.py
+++ backend/applications/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Application
+from jobs.models import Job
+from jobs.serializers import JobSerializer
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    applicant = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    job = JobSerializer(read_only=True)
+    job_id = serializers.PrimaryKeyRelatedField(
+        queryset=Job.objects.all(), write_only=True, source='job'
+    )
+
     class Meta:
         model = Application
-        fields = '__all__'
-        read_only_fields = ['id', 'applicant', 'applied_at']
+        fields = ['id', 'job', 'job_id', 'applicant', 'status', 'applied_at', 'cover_letter']
+        read_only_fields = ['id', 'applicant', 'applied_at', 'job']
