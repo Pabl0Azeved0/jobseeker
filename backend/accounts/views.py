87eb2eed4fff354c06fd0efb130f52ec7caec701
Pablo Azevedo <pabloslayer@gmail.com>
2025-03-27 16:21:46 -0300

    feat(BE:profiles): Update profiles to be able to receive new fields on profile

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,25 +1,28 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, status
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer, UserSignupSerializer
 from profiles.models import Profile
 from django.core.mail import send_mail
 from django.conf import settings
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+
 User = get_user_model()
 
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 class UserDetailView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 class SignupView(generics.CreateAPIView):
     serializer_class = UserSignupSerializer
-    permission_classes = []
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
--- backend/profiles/serializers.py
+++ backend/profiles/serializers.py
@@ -2,8 +2,13 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
-    resume = serializers.FileField(required=False, allow_null=True)
-
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = ['id', 'user', 'bio', 'skills', 'contact', 'resume', 'location', 'birth_date']
+        read_only_fields = ['id', 'user']
+
+    def validate(self, attrs):
+        for field in ['bio', 'skills', 'contact', 'location']:
+            if attrs.get(field) is None:
+                attrs[field] = ''
+        return attrs
