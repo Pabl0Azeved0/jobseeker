22db976d10f67c9a6692b6b44da9f894e695aba2
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-18 18:43:26 -0300

    hotfix(BE:accounts): Lint fix #discard 4

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignupView, UserListView, UserDetailView
 
 urlpatterns = [
-    path('users/', UserListView.as_view(), name='user-list'),
-    path('users/<uuid:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('signup/', SignupView.as_view(), name='signup'),
+    path("users/", UserListView.as_view(), name="user-list"),
+    path("users/<uuid:pk>/", UserDetailView.as_view(), name="user-detail"),
+    path("signup/", SignupView.as_view(), name="signup"),
 ]
