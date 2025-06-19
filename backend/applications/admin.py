673c833e07c8a3ca4c4cffb448bf87110b35fcd0
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-19 18:32:04 -0300

    hotfix(BE:application): Lint fix #discard 1

--- backend/applications/admin.py
+++ backend/applications/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Application
 
+
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ['job', 'applicant', 'status']
-    search_fields = ['job__title', 'applicant__username']
-    list_filter = ['status']
+    list_display = ["job", "applicant", "status"]
+    search_fields = ["job__title", "applicant__username"]
+    list_filter = ["status"]
