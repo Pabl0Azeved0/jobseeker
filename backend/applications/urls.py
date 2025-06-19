fda5ea50c07261fdd0d4f3c08bd693e02cd41c0c
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-19 18:32:46 -0300

    hotfix(BE:application): Lint fix #discard 5

--- backend/applications/urls.py
+++ backend/applications/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from .views import ApplicationListCreateView, ApplicationDetailView
 
 urlpatterns = [
-    path('applications/', ApplicationListCreateView.as_view(), name='application-list-create'),
-    path('applications/<uuid:pk>/', ApplicationDetailView.as_view(), name='application-detail'),
+    path(
+        "applications/",
+        ApplicationListCreateView.as_view(),
+        name="application-list-create",
+    ),
+    path(
+        "applications/<uuid:pk>/",
+        ApplicationDetailView.as_view(),
+        name="application-detail",
+    ),
 ]
