072b9e98f315b1f9b6acc4555b7a6c08c215bf3d
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-20 15:57:24 -0300

    hotfix(BE:jobs): Lint fix #discard 3

--- backend/jobs/documents.py
+++ backend/jobs/documents.py
@@ -1,23 +1,26 @@
 from django_elasticsearch_dsl import Document, Index, fields
 from .models import Job
 
-job_index = Index('jobs')
+job_index = Index("jobs")
+
 
 @job_index.doc_type
 class JobDocument(Document):
-    posted_by = fields.ObjectField(properties={
-        'id': fields.TextField(),
-        'username': fields.TextField(),
-        'email': fields.TextField(),
-    })
+    posted_by = fields.ObjectField(
+        properties={
+            "id": fields.TextField(),
+            "username": fields.TextField(),
+            "email": fields.TextField(),
+        }
+    )
 
     class Django:
         model = Job
         fields = [
-            'id',
-            'title',
-            'description',
-            'location',
-            'salary',
-            'created_at',
+            "id",
+            "title",
+            "description",
+            "location",
+            "salary",
+            "created_at",
         ]
