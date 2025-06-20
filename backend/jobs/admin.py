636987e7f2a04990082b9a1650595ae96346c630
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-20 15:57:02 -0300

    hotfix(BE:jobs): Lint fix #discard 1

--- backend/jobs/admin.py
+++ backend/jobs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Job
 
+
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    list_display = ['title', 'id']
-    search_fields = ['title', 'description']
+    list_display = ["title", "id"]
+    search_fields = ["title", "description"]
