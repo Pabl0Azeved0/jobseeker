22fdd33f497240d7d074d5bffa9c4e80acd2cb88
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-20 15:57:37 -0300

    hotfix(BE:jobs): Lint fix #discard 4

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.conf import settings
 
+
 class Job(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
