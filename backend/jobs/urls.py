29df6dbafbae2cd21f41a419cf9b797212d17576
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-20 15:58:06 -0300

    hotfix(BE:jobs): Lint fix #discard 6

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import JobListCreateView, JobDetailView, JobSearchView
 
 urlpatterns = [
-    path('jobs/', JobListCreateView.as_view(), name='job-list-create'),
-    path('jobs/<uuid:pk>/', JobDetailView.as_view(), name='job-detail'),
-    path('jobs/search/', JobSearchView.as_view(), name='job-search'),
+    path("jobs/", JobListCreateView.as_view(), name="job-list-create"),
+    path("jobs/<uuid:pk>/", JobDetailView.as_view(), name="job-detail"),
+    path("jobs/search/", JobSearchView.as_view(), name="job-search"),
 ]
