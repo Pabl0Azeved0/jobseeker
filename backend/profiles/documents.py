d0572273f6b54bb40c84a118486919b815a0df96
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-22 09:34:12 -0300

    hotfix(BE:profiles): Lint fix #3

--- backend/profiles/documents.py
+++ backend/profiles/documents.py
@@ -1,19 +1,22 @@
 from django_elasticsearch_dsl import Document, Index, fields
 from .models import Profile
 
-profile_index = Index('profiles')
+profile_index = Index("profiles")
+
 
 @profile_index.doc_type
 class ProfileDocument(Document):
-    user = fields.ObjectField(properties={
-        'id': fields.TextField(),
-        'username': fields.TextField(),
-        'email': fields.TextField(),
-    })
+    user = fields.ObjectField(
+        properties={
+            "id": fields.TextField(),
+            "username": fields.TextField(),
+            "email": fields.TextField(),
+        }
+    )
 
     class Django:
         model = Profile
         fields = [
-            'id',
-            'bio',
+            "id",
+            "bio",
         ]
