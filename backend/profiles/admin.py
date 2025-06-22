699c6f55cd5f3a7cd3b468bae3239329d04c311f
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-22 09:33:55 -0300

    hotfix(BE:profiles): Lint fix #1

--- backend/profiles/admin.py
+++ backend/profiles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'bio']
-    search_fields = ['user__username', 'bio']
+    list_display = ["user", "bio"]
+    search_fields = ["user__username", "bio"]
