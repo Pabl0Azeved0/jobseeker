681bf814345e8a99d40ae98685296243b69ff343
Pablo Azevedo <pabloslayer@gmail.com>
2025-06-23 08:53:09 -0300

    hotfix(BE:profiles): Lint fix #6

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ProfileListCreateView, ProfileDetailView, ProfileSearchView, MyProfileView
+from .views import (
+    ProfileListCreateView,
+    ProfileDetailView,
+    ProfileSearchView,
+    MyProfileView,
+)
 
 urlpatterns = [
-    path('profiles/', ProfileListCreateView.as_view(), name='profile-list-create'),
-    path('profiles/<uuid:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
-    path('profiles/search/', ProfileSearchView.as_view(), name='profile-search'),
-    path('profiles/me/', MyProfileView.as_view(), name='my-profile'),
+    path("profiles/", ProfileListCreateView.as_view(), name="profile-list-create"),
+    path("profiles/<uuid:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
+    path("profiles/search/", ProfileSearchView.as_view(), name="profile-search"),
+    path("profiles/me/", MyProfileView.as_view(), name="my-profile"),
 ]
